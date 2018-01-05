06f5278f0f122515d5f2dae74383d35786342ad9
Waydesss <Waydeslilbro@Gmail.com>
2018-01-05 16:02:45 -0700

    home

---
+++ Assets/Scripts/UI/Buttons.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class buttons : MonoBehaviour {
+
+public void PlayButton (int scene)
+	{
+		
+		Application.LoadLevel (scene);
+
+	}
+
+public void ExitButton ()
+		
+	{
+		
+		Application.Quit ();
+	}
+
+}
